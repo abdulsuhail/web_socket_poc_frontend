35d87121847fbe8990c5f301e62fad5a30441f74
abdulsuhail <abdulsuhail471@gmail.com>
2021-12-02 11:49:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://localhost:4001"
+    };
+  }
+componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint, {
+      withCredentials: true,
+      extraHeaders: {
+        "my-custom-header": "abcd"
+      }
+    });
+    socket.on("FromAPI", data => {
+      console.log("Dtaasdas")
+      this.setState({ response: data })
+    })
+    ;
+  }
+render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? <p>
+              The temperature in Florence is: {response} °F
+            </p>
+          : <p>Loading...</p>}
+      </div>
+    );
+  }
 }
-
 export default App;
